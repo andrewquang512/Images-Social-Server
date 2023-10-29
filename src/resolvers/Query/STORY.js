6d61fd2e77c6a44a0fc2b63ae5da32b73e46a656
andrewquang512 <andrew.viquang@gmail.com>
2023-10-29 14:59:34 +0700

    Update pagination for getNewStories query

--- src/resolvers/Query/STORY.js
+++ src/resolvers/Query/STORY.js
@@ -9,22 +9,53 @@ const storyQuery = {
       throw e;
     }
   },
+
+  /**
+   *
+   * @param {*} parent
+   * @param {{limit: number, after: string}} args
+   * @param {*} info
+   * @returns
+   */
   getNewStories: async (parent, args, { prisma }, info) => {
     const { limit, after } = args;
 
-    try {
-      return await prisma.story.findMany({
+    const [result, count] = await prisma.$transaction([
+      prisma.story.findMany({
         take: limit || DEFAULT_LIMIT,
+        orderBy: {
+          createdAt: 'desc',
+        },
         ...(after && {
           cursor: {
             id: after,
           },
         }),
-      });
-    } catch (e) {
-      console.log(e);
-      throw e;
-    }
+      }),
+      prisma.story.count(),
+    ]);
+
+    console.log('Result', result);
+    console.log('count', count);
+
+    const hasNextPage = result.length === 0 || result.length <= count;
+    // console.log(nodes.slice(-1));
+    console.log({ hasNextPage });
+
+    const nodes = result.map((each) => ({
+      node: each,
+      cursor: each.id,
+    }));
+    return {
+      edges: nodes,
+      pageInfo: {
+        hasNextPage,
+        hasPreviousPage: after ? true : false,
+        // startCursor,
+        startCursor: nodes.length === 0 ? '' : nodes[0].cursor,
+        endCursor: nodes.length === 0 ? '' : nodes.slice(-1)[0].cursor,
+      },
+    };
   },
   storyInfo: async (parent, args, { prisma }, info) => {
     return await prisma.story.findUnique({
