8b8e0d71b223f3711105e9a6316e2618f1d0b82d
andrewquang512 <andrew.viquang@gmail.com>
2023-11-19 19:13:33 +0700

    Update hamming distance algorithm

--- src/resolvers/Common/compareImages.js
+++ src/resolvers/Common/compareImages.js
@@ -1,9 +1,18 @@
+import Jimp from 'Jimp';
+
 export function compareImages(image1hash, image2hash) {
   // Perceived distance
-  const hammingDistance = findHammingDistance(image1hash, image2hash);
-  console.log(`compareImages: hamming distance: ${hammingDistance.toFixed(3)}`);
+  const vihammingDistance = findHammingDistance(image1hash, image2hash);
+  const jimphammingDistance = Jimp.compareHashes(image1hash, image2hash);
 
-  if (hammingDistance <= 10) {
+  console.log(
+    `compareImages: vihammingDistance distance: ${vihammingDistance}`,
+  );
+  console.log(
+    `compareImages: jimphammingDistance distance: ${jimphammingDistance}`,
+  );
+
+  if (vihammingDistance <= 10 && jimphammingDistance < 0.15) {
     console.log('compareImages: Images match!');
     return true;
   }
@@ -23,3 +32,41 @@ const findHammingDistance = (str1 = '', str2 = '') => {
   }
   return 0;
 };
+//
+// const findLevenDistance = (a, b) => {
+//   if (a.length == 0) return b.length;
+//   if (b.length == 0) return a.length;
+//
+//   var matrix = [];
+//
+//   // increment along the first column of each row
+//   var i;
+//   for (i = 0; i <= b.length; i++) {
+//     matrix[i] = [i];
+//   }
+//
+//   // increment each column in the first row
+//   var j;
+//   for (j = 0; j <= a.length; j++) {
+//     matrix[0][j] = j;
+//   }
+//
+//   // Fill in the rest of the matrix
+//   for (i = 1; i <= b.length; i++) {
+//     for (j = 1; j <= a.length; j++) {
+//       if (b.charAt(i - 1) == a.charAt(j - 1)) {
+//         matrix[i][j] = matrix[i - 1][j - 1];
+//       } else {
+//         matrix[i][j] = Math.min(
+//           matrix[i - 1][j - 1] + 1, // substitution
+//           Math.min(
+//             matrix[i][j - 1] + 1, // insertion
+//             matrix[i - 1][j] + 1,
+//           ),
+//         ); // deletion
+//       }
+//     }
+//   }
+//
+//   return matrix[b.length][a.length];
+// };
--- src/resolvers/Common/hashImage.js
+++ src/resolvers/Common/hashImage.js
@@ -1,4 +1,4 @@
-import Jimp from 'jimp';
+import Jimp from 'Jimp';
 
 export async function hashImage(imageUrl) {
   try {
